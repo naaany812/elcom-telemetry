4d08d80ca0fa385bd55cdae37082ee02457f778c
Restyled.io <commits@restyled.io>
2020-05-22 06:52:31 +0000

    Restyled by pg_format

--- src/main/resources/db/migration/V2__change_device_type_in_data_climate_table.sql
+++ src/main/resources/db/migration/V2__change_device_type_in_data_climate_table.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.data_climate
-ALTER COLUMN device_type TYPE varchar(5);
+ALTER TABLE public.data_climate ALTER COLUMN device_type TYPE varchar(5);
+
--- src/main/resources/db/migration/V3__change_device_type_in_data_energy_table.sql
+++ src/main/resources/db/migration/V3__change_device_type_in_data_energy_table.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.data_energy
-ALTER COLUMN device_type TYPE varchar(5);
+ALTER TABLE public.data_energy ALTER COLUMN device_type TYPE varchar(5);
+
